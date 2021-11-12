914b4aa5e40de77f0c1c777718f0ee77718ea8bb
EAghayev <eaghayevv@gmail.com>
2021-11-12 16:27:15 +0400

    project added

---
+++ 12112021/12112021/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+
+namespace _12112021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-GB");
+
+            string path = @"C:\Users\Eagha\Desktop\CodeLessons\P201";
+
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+            Console.WriteLine(dirInfo.Exists);
+            Console.WriteLine(dirInfo.Name);
+            Console.WriteLine(dirInfo.FullName);
+            Console.WriteLine(dirInfo.CreationTime);
+            Console.WriteLine(dirInfo.LastAccessTime);
+            Console.WriteLine(dirInfo.LastWriteTime);
+
+            Console.WriteLine("sub files:");
+            foreach (var item in dirInfo.GetFiles())
+            {
+                if (item.LastWriteTime.Date == DateTime.Now.Date)
+                {
+                    item.Delete();
+                }
+                Console.WriteLine($"{item.Name} - {item.CreationTime} - {item.LastAccessTime} - {item.LastWriteTime}");
+            }
+
+            Console.WriteLine("sub folders:");
+            foreach (var item in dirInfo.GetDirectories())
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("\n===========================================\n");
+
+            Console.WriteLine("all sub directories:");
+            foreach (var item in dirInfo.GetDirectories())
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine("\t files:");
+                foreach (var file in item.GetFiles())
+                {
+                    Console.WriteLine("\t "+file.Name + " "+file.Length/1024);
+                }
+            }
+
+            Console.WriteLine("\n===========================================\n");
+
+            FileInfo newFileInfo = new FileInfo(path + @"\test.txt");
+
+            if(!newFileInfo.Exists)
+            {
+                var newFileStream = newFileInfo.Create();
+                newFileStream.Close();
+            }
+
+
+            string[] content = new string[]
+            {
+                "Hello",
+                "Salam",
+                "Merhaba"
+            };
+
+            File.WriteAllLines(newFileInfo.FullName, content);
+
+            foreach (var file in dirInfo.GetFiles())
+            {
+                Console.WriteLine("lines: ");
+                foreach (var line in File.ReadAllLines(file.FullName))
+                {
+                    Console.WriteLine("\t " + line);
+                }
+            }
+
+
+            Console.WriteLine("\n===========================================\n");
+
+            //User user = new User
+            //{
+            //    Name = "Hikmet",
+            //    Surname = "Abbasov",
+            //    Age = 90
+            //};
+
+            //SerializeBinary(user);
+
+            User existUser = DeserializeBinary();
+            Console.WriteLine(existUser);
+        }
+
+        public static void SerializeBinary(User user)
+        {
+            Stream stream = new FileStream(@"C:\Users\Eagha\Desktop\User.dat", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream,user);
+        }
+
+        public static User DeserializeBinary()
+        {
+            Stream stream = new FileStream(@"C:\Users\Eagha\Desktop\User.dat", FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            var data = (User)bf.Deserialize(stream);
+            return data;
+        }
+    }
+}
---
+++ 12112021/12112021/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace _12112021
+{
+    [Serializable]
+    class User:ISerializable
+    {
+        public string Name;
+        public string Surname;
+        public byte Age;
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name, typeof(string));
+            info.AddValue("Surname", Surname, typeof(string));
+            info.AddValue("Age", Age, typeof(byte));
+        }
+        public User()
+        {
+
+        }
+        public User(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            Surname = (string)info.GetValue("Surname", typeof(string));
+            Age = (byte)info.GetValue("Age", typeof(byte));
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname} - {Age}";
+        }
+    }
+}
